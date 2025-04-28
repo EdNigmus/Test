a940d6588c7114af672e66b66ad4b9af7c849acb
EdNigmus <ed.nigmus@gmail.com>
2025-04-28 13:47:15 +0200

    Création de listes en python

--- Liste.py
+++ Liste.py
@@ -1,1 +1,7 @@
 # Créer une liste
+# En Python, les listes sont des structures de données ordonnées, modifiables (mutables) et hétérogènes (elles peuvent
+# contenir diffrents types d’objets). Elles sont très utilisées pour stocker et manipuler des ensembles de données.
+numb_lst = [1, 2, 3.52, 4]
+str_lst = ["Laurent", "Avion", "Salade"]
+bol_lst = [True, False]
+mixte_lst = [52, "texte", True, 3.14159]
