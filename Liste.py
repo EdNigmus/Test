ae009757286effc137a3023bfd893ddae179a8c9
EdNigmus <ed.nigmus@gmail.com>
2025-04-28 13:58:15 +0200

    modifier structure fichier "Liste.py"

--- Liste.py
+++ Liste.py
@@ -1,6 +1,10 @@
-# Créer une liste
+# Chatgpt : les listes en python
+#===============================
+
 # En Python, les listes sont des structures de données ordonnées, modifiables (mutables) et hétérogènes (elles peuvent
 # contenir diffrents types d’objets). Elles sont très utilisées pour stocker et manipuler des ensembles de données.
+
+# Créer une liste
 numb_lst = [1, 2, 3.52, 4]
 str_lst = ["Laurent", "Avion", "Salade"]
 bol_lst = [True, False]
@@ -13,5 +17,5 @@ print()
 # Accéder et afficher un élément d'une liste 
 print(mixte_lst[3]) # affichera "3.14159"
 print(mixte_lst[0]) # affichera le 1er élément de la liste, soit "52"
-print(str_lst[-1]) # affichera le dernier élément de la liste
+print(str_lst[-1]) # affichera le dernier élément de la liste, ici "Salade"
 
