65467f12354c855287860147257136c678319293
EdNigmus <ed.nigmus@gmail.com>
2025-04-29 08:15:02 +0200

    Ajouter, méthode "insert()" un élément à un indice spécifique

--- Liste.py
+++ Liste.py
@@ -13,29 +13,31 @@ mixte_lst = [52, "texte", True, 3.14159]
 
 # AFFICHER une liste contenue de la variable "numb_lst" 
 print("Afficher le contenu de 'numb_lst'")
-print (numb_lst)
-print()
+print (numb_lst,"\n")
 
 # ACCEDER et afficher un élément d'une liste 
 # l'élément que l'on veut afficher ici est indiqué entre crochet. Se sera l'élément à l'index tant... que l'on veut afficher contenu dans une variable.
 print("Accéder et afficher un élément d'une liste")
 print(mixte_lst[3]) # affichera "3.14159"
 print(mixte_lst[0]) # affichera le 1er élément de la liste, soit "52"
-print(str_lst[-1]) # affichera le dernier élément de la liste, ici "Salade"
-print()
+print(str_lst[-1],"\n") # affichera le dernier élément de la liste, ici "Salade"
 
 # MODIFIER un élément de la liste
 # ici dans la liste contenue dans la variable "str_lst" à l'index 0 qui est le 1er élément, je le remplace par la string "Béatrice"
 # L'index 0 est "Laurent" qui sera donc remplacé par "Béatrice"
 print("Modifier une élément de la liste 'str_lst'")
 str_lst[0] = "Béatrice" 
-print(str_lst)
-print()
+print(str_lst,"\n")
 
 # AJOUTER "append()" des éléments à une liste existante
-print("Ajouter des éléments à la liste 'Tarte_pommes'")
+print("Méthode 'append()', ajouter des éléments à la fin de la liste 'Tarte_pommes'")
 Tarte_pommes = [] # Ici la liste est vide, mais existe. 
 print(Tarte_pommes, "<- ici la liste est vide") 
 # Pour ajouter un élément dans la liste on utilise la méthode 'append()' qui ajoute un élément à la fin de la liste.
 Tarte_pommes.append("pommes")
-print(Tarte_pommes)
+print(Tarte_pommes,"\n")
+
+# AJOUTER "insert()" des éléments à un indice spécifique
+print("Méthode 'insert()', ajoute des éléments à un indice spécifique dans la liste")
+Tarte_pommes.insert(1, "farine") # on aurait pu aussi utiliser la méthode append() car on ajouter un indice en fin de liste
+print(Tarte_pommes,"\n")
